076c6417b7e44f7bd924a179c865bfb677fed6b1
Michael Harrison <harrison.michael@bitsourceky.com>
2018-09-04 09:52:15 -0400

    Initial commit

---
+++ src/main/java/io/mikesthings/demodatabase/courses/CourseController.java
@@ -0,0 +1,43 @@
+package io.mikesthings.demodatabase.courses;
+
+import io.mikesthings.demodatabase.topics.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("/topics/{id}/courses")
+    public List<Course> getAllCourses(@PathVariable String id) {
+        return courseService.getAllCourses(id);
+    }
+
+    @RequestMapping("/topics/{topicId}/courses/{id}")
+    public Optional<Course> getCourse(@PathVariable String id){
+        return courseService.getCourse(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId){
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.addCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String topicId,
+                             @PathVariable String id){
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.updateCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{topicId}/course/{id}")
+    public void deleteCourse(@PathVariable String id){
+        courseService.deleteCourse(id);
+    }
+
+}
